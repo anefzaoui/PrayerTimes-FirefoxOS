57e76c0916f532ae710b45735eb3caa30a398ea5
Ahmed Nefzaoui <nefzaoui.ahmed@hotmail.com>
2013-07-26 23:14:30 +0100

    init

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ Prayer Times App
 ========
 
 For Firefox OS, App that shows Prayer times.
+
+Target version: 0.1
+
+Current Version : 0.0.1
+
+Features for v0.1
+	
+	- Basic Prayer Times default display at main screen.
+	- GPS Button to relocate yourself.
+	- Manually Choosing the city desired.
+	- Previewing Prayer times for a future or past date.
--- js/utils.js
+++ js/utils.js
@@ -1,53 +1,62 @@
-	var PT = {
+var PT = {
+
 	AllSet : false,
+
 	tz:null,
+
 	longitude:null,
+
 	latitude:null,
+
 	lastTimeChecked : null,
+
 	date : null,
+
 	times : null,
+
 	currentdate : null,
+
 	datetime:null,
 	
 	calculate : function(){
-	PT.AllSet = LocalData.getData("AllSet");
-	var weekday=new Array(7);
-	weekday[0]="Sunday";
-	weekday[1]="Monday";
-	weekday[2]="Tuesday";
-	weekday[3]="Wednesday";
-	weekday[4]="Thursday";
-	weekday[5]="Friday";
-	weekday[6]="Saturday";
-
-	this.currentdate = new Date(); 
-	this.datetime = "Today is: "
+		PT.AllSet = LocalData.getData("AllSet");
+		var weekday=new Array(7);
+		weekday[0]="Sunday";
+		weekday[1]="Monday";
+		weekday[2]="Tuesday";
+		weekday[3]="Wednesday";
+		weekday[4]="Thursday";
+		weekday[5]="Friday";
+		weekday[6]="Saturday";
+		this.currentdate = new Date(); 
+		this.datetime = "Today is: "
 				+ weekday[this.currentdate.getDay()] + " "
 				+ this.currentdate.getDate() + "/"
                 + (this.currentdate.getMonth()+1)  + "/" 
                 + this.currentdate.getFullYear();
                 
-	if(PT.AllSet){
-	PT.latitude = LocalData.getData("latitude");
-	PT.longitude = LocalData.getData("longitude");
-	PT.tz = LocalData.getData("tz");
+		if(PT.AllSet){
+		PT.latitude = LocalData.getData("latitude");
+		PT.longitude = LocalData.getData("longitude");
+		PT.tz = LocalData.getData("tz");
 	
-	this.times = prayTimes.getTimes(new Date(), [PT.latitude, PT.longitude], PT.tz);
-	document.getElementById('now').innerHTML = this.datetime;
-	document.getElementById('fajr').innerHTML = this.times['fajr'];
-	document.getElementById('dhuhr').innerHTML = this.times['dhuhr'];
-	document.getElementById('asr').innerHTML = this.times['asr'];
-	document.getElementById('maghrib').innerHTML = this.times['maghrib'];
-	document.getElementById('isha').innerHTML = this.times['isha'];
-	}
-	else{
-	PT.refresh();
-	}
+		this.times = prayTimes.getTimes(new Date(), [PT.latitude, PT.longitude], PT.tz);
+		document.getElementById('now').innerHTML = this.datetime;
+		document.getElementById('fajr').innerHTML = this.times['fajr'];
+		document.getElementById('dhuhr').innerHTML = this.times['dhuhr'];
+		document.getElementById('asr').innerHTML = this.times['asr'];
+		document.getElementById('maghrib').innerHTML = this.times['maghrib'];
+		document.getElementById('isha').innerHTML = this.times['isha'];
+		}
+		else{
+		PT.refresh();
+		}
 	},
 	
+	
 	refresh: function(){
-	navigator.geolocation.getCurrentPosition(locationSuccess, locationFail);
-    function locationSuccess(position) {
+		navigator.geolocation.getCurrentPosition(locationSuccess, locationFail);
+		function locationSuccess(position) {
         PT.latitude = position.coords.latitude;
 		PT.longitude = position.coords.longitude;
 		PT.lastTimeChecked = new Date();
@@ -60,10 +69,10 @@
 		LocalData.storeData("tz",PT.tz);
 		LocalData.storeData("AllSet",PT.AllSet);
 		PT.calculate();
-    }
-    function locationFail() {
-        alert("Oops, could not find you.");
-    }
+		}
+		function locationFail() {
+			alert("Oops, could not find you.");
+		}
 	
 	}
 	
